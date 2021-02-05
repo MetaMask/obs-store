566d07636173d9bc333debbbd97db08a1ea7775d
Erik Marks <rekmarks@protonmail.com>
2021-02-05 10:33:33 -0800

    Delete LocalStorageStore

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from './asStream';
 export * from './ComposedStore';
-export * from './LocalStorageStore';
 export * from './MergedStore';
 export * from './ObservableStore';
 export * from './transform';
