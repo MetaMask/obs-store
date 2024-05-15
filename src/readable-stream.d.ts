5d43cded0c61838dba8c8bc60e586ed83874de5f
legobeat <109787230+legobeat@users.noreply.github.com>
2024-05-16 06:14:27 +0900

    feat: Allow overriding default Stream options (#110)
    Allow overriding stream constructor options in storeTransformStream
    Allow overriding stream constructor options in ObservableStoreStream constructor
    Allow overriding stream constructor options in storeAsStream function

--- src/readable-stream.d.ts
+++ src/readable-stream.d.ts
@@ -1,4 +1,5 @@
 // eslint-disable import/unambiguous
 declare module 'readable-stream' {
   export { Duplex, Transform, Writable, pipeline } from 'stream';
+  export type { DuplexOptions, TransformOptions } from 'stream';
 }
